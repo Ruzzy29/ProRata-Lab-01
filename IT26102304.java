f9a16db6890e98001a9338d1c26e1b3542183a07
IT26102304 <it26102304@my.sliit.lk>
2026-09-25 19:00:49 +0530

    Add files via upload

---
+++ IT26102304.java
@@ -0,0 +1,18 @@
+public class IT26102304 {
+	
+	public static void main (String[] args){
+		
+		System.out.println("Ruzaidh Rizvi");
+		System.out.println ("21");
+		System.out.println ("Matale");
+	}
+	
+	
+	
+	
+}
+
+
+
+         
+		  
